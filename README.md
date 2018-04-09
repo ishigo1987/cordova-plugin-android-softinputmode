47ab1591d6a9dbddc43a333452d3f4ee7bc39ec0
sekar_neutrinos <gnanasekaran.perumal@neutrinos.co>
2018-04-09 12:17:48 +0530

    more softinput mode options added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This plugin lets you change the [android:windowSoftInputMode property](https://developer.android.com/guide/topics/manifest/activity-element.html#wsoft) at runtime.
 
-Currently only `adjustPan`, `adjustResize` and `adjustNothing` values are supported.
+Currently `adjustPan`, `adjustResize`, `adjustNothing`, `stateUnspecified`, `stateUnchanged`, `stateHidden`, `stateAlwaysHidden`, `stateVisible`, `stateAlwaysVisible`, `adjustUnspecified` values are supported.
 
 ## Example
 After the `deviceready` event:
--- src/SoftInputMode.java
+++ src/SoftInputMode.java
@@ -13,31 +13,43 @@ import org.json.JSONException;
  */
 public class SoftInputMode extends CordovaPlugin {
 
-   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if (action.equals("set")) {
+      final String value = args.getString(0);
+      this.set(value, callbackContext);
+      return true;
     }
+    return false;
+  }
 
-	@Override
-	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		if (action.equals("set")) {
-			final String value = args.getString(0);
-			this.set(value, callbackContext);
-			return true;
-		}
-		return false;
-	}
-
-	private void set(final String value, final CallbackContext callbackContext) {
+  private void set(final String value, final CallbackContext callbackContext) {
     cordova.getActivity().runOnUiThread(new Runnable() {
       public void run() {
         int mode;
-        if(value.equals("adjustNothing"))
+        if (value.equals("adjustNothing"))
           mode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING;
-        else if(value.equals("adjustResize"))
+        else if (value.equals("adjustResize"))
           mode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE;
-        else if(value.equals("adjustPan"))
+        else if (value.equals("adjustPan"))
           mode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN;
-        else {
+        else if (value.equals("stateUnspecified")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNSPECIFIED;
+        } else if (value.equals("stateUnchanged")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED;
+        } else if (value.equals("stateHidden")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
+        } else if (value.equals("stateAlwaysHidden")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN;
+        } else if (value.equals("stateAlwaysVisible")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE;
+        } else if (value.equals("adjustUnspecified")) {
+          mode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_UNSPECIFIED;
+        } else {
           callbackContext.error("Invalid mode");
           return;
         }
@@ -45,6 +57,6 @@ public class SoftInputMode extends CordovaPlugin {
         cordova.getActivity().getWindow().setSoftInputMode(mode);
         callbackContext.success("Success");
       }
-	  });
+    });
   }
 }
